6b4748d75a6319291c1ec32d1ef9667bd4ab88d1
shailesh <shaileshssavani107@gmail.com>
2025-05-08 01:24:54 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 console.log(process.env.MONGO_HOST);
-const url = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
+const url = process.env.MONGO_URL;
 
 
 const connection = new MongoClient(url);
